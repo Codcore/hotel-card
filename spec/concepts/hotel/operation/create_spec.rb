568fb233dc11561aca54582c67050b757c30d602
Andrew Y <codcore@gmail.com>
2020-04-20 11:29:57 +0300

    Update specs for Hotel::Operation::Create

--- spec/concepts/hotel/operation/create_spec.rb
+++ spec/concepts/hotel/operation/create_spec.rb
@@ -1,18 +1,27 @@
 require 'rails_helper'
 
 describe Hotel::Operation::Create do
-  let(:params) { { title: 'Title', address: 'Address' } }
+  let(:tags) { [ 'Test tag 1', 'Test tag 2' ] }
+  let(:params) { { title: 'Title', address: 'Address', tags: tags } }
   subject { described_class.(params: params) }
 
   it 'result is of Result class' do
     is_expected.to be_instance_of Trailblazer::Operation::Railway::Result
   end
 
-  it 'result should contain Hotel record' do
+  it 'result contains Hotel record' do
     expect(subject[:model]).to be_instance_of ::Hotel
   end
 
-  it 'should create an Hotel record' do
+  it 'creates an Hotel record' do
     expect { subject }.to change(::Hotel, :count).by(1)
+  end
+
+  it 'assigns tags to Hotel record' do
+    expect(subject[:model].tag_list).to eq tags
+  end
+
+  it 'assigns default image to Hotel record' do
+    expect(subject[:model].image.filename.to_s).to eq 'hotel.jpeg'
   end
 end
